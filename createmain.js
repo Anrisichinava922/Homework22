42c346575d86d3bd2b866fac4f6adf6588379029
Anrisichinava922 <anrisichinava922@gmail.com>
2023-12-09 16:12:00 +0400

    Homework22

--- createmain.js
+++ createmain.js
@@ -16,6 +16,12 @@ const arrow4 = document.querySelector('.arrow4')
 const menu5 = document.querySelector('.item5')
 const variantebi5 = document.querySelector('.variantebi5')
 const arrow5 = document.querySelector('.arrow5')
+const res1 = document.querySelector('.sp1')
+const res2 = document.querySelector('.sp2')
+const res3 = document.querySelector('.sp3')
+const res4 = document.querySelector('.sp4')
+const res5 = document.querySelector('.sp5')
+
 
 
 icon.addEventListener('click', () =>{
@@ -84,74 +90,150 @@ airchie1.addEventListener('click', ()=>{
     airchie1.classList.toggle('gamwvanda')
     airchie2.classList.remove('gamwvanda')
     airchie3.classList.remove('gamwvanda')
+    if (airchie1.classList.contains('gamwvanda')) {
+        res1.textContent = 'Capsule';
+    } else {
+        res1.textContent = '';
+    }
 })
 airchie2.addEventListener('click', ()=>{
     airchie2.classList.toggle('gamwvanda')
     airchie1.classList.remove('gamwvanda')
     airchie3.classList.remove('gamwvanda')
+    if(airchie2.classList.contains('gamwvanda')){
+        res1.textContent = '"Filter"'
+    }else{
+        res1.textContent = ""
+    }
 })
 airchie3.addEventListener('click', ()=>{
     airchie3.classList.toggle('gamwvanda')
     airchie2.classList.remove('gamwvanda')
     airchie1.classList.remove('gamwvanda')
+    if(airchie3.classList.contains('gamwvanda')){
+        res1.textContent = '"Espresso"'
+    }else{
+        res1.textContent = ""
+    }
 })
 airchie4.addEventListener('click', ()=>{
     airchie4.classList.toggle('gamwvanda')
     airchie5.classList.remove('gamwvanda')
     airchie6.classList.remove('gamwvanda')
+    if(airchie4.classList.contains('gamwvanda')){
+        res2.textContent = '"Single Origin"'
+    }else{
+        res2.textContent = ""
+    }
 })
 airchie5.addEventListener('click', ()=>{
     airchie5.classList.toggle('gamwvanda')
     airchie4.classList.remove('gamwvanda')
     airchie6.classList.remove('gamwvanda')
+    if(airchie5.classList.contains('gamwvanda')){
+        res2.textContent = '"Decaf"'
+    }else{
+        res2.textContent = ""
+    }
 })
 airchie6.addEventListener('click', ()=>{
     airchie6.classList.toggle('gamwvanda')
     airchie5.classList.remove('gamwvanda')
     airchie4.classList.remove('gamwvanda')
+    if(airchie6.classList.contains('gamwvanda')){
+        res2.textContent = '"Blended"'
+    }else{
+        res2.textContent = ""
+    }
 })
 airchie7.addEventListener('click', ()=>{
     airchie7.classList.toggle('gamwvanda')
     airchie8.classList.remove('gamwvanda')
     airchie9.classList.remove('gamwvanda')
+    if(airchie7.classList.contains('gamwvanda')){
+        res3.textContent = '"250g"'
+    }else{
+        res3.textContent = ""
+    }
 })
 airchie8.addEventListener('click', ()=>{
     airchie8.classList.toggle('gamwvanda')
     airchie7.classList.remove('gamwvanda')
     airchie9.classList.remove('gamwvanda')
+    if(airchie8.classList.contains('gamwvanda')){
+        res3.textContent = '"500g"'
+    }else{
+        res3.textContent = ""
+    }
 })
 airchie9.addEventListener('click', ()=>{
     airchie9.classList.toggle('gamwvanda')
     airchie8.classList.remove('gamwvanda')
     airchie7.classList.remove('gamwvanda')
+    if(airchie9.classList.contains('gamwvanda')){
+        res3.textContent = '"1000g"'
+    }else{
+        res3.textContent = ""
+    }
 })
 airchie10.addEventListener('click', ()=>{
     airchie10.classList.toggle('gamwvanda')
     airchie11.classList.remove('gamwvanda')
     airchie12.classList.remove('gamwvanda')
+    if(airchie10.classList.contains('gamwvanda')){
+        res4.textContent = '"Wholebean"'
+    }else{
+        res4.textContent = ""
+    }
 })
 airchie11.addEventListener('click', ()=>{
     airchie11.classList.toggle('gamwvanda')
     airchie10.classList.remove('gamwvanda')
     airchie12.classList.remove('gamwvanda')
+    if(airchie11.classList.contains('gamwvanda')){
+        res4.textContent = '"Filter"'
+    }else{
+        res4.textContent = ""
+    }
 })
 airchie12.addEventListener('click', ()=>{
     airchie12.classList.toggle('gamwvanda')
     airchie11.classList.remove('gamwvanda')
     airchie10.classList.remove('gamwvanda')
+    if(airchie12.classList.contains('gamwvanda')){
+        res4.textContent = '"Cafetiere"'
+    }else{
+        res4.textContent = ""
+    }
 })
 airchie13.addEventListener('click', ()=>{
     airchie13.classList.toggle('gamwvanda')
     airchie14.classList.remove('gamwvanda')
     airchie15.classList.remove('gamwvanda')
+    if(airchie13.classList.contains('gamwvanda')){
+        res5.textContent = '"Every week"'
+    }else{
+        res5.textContent = ""
+    }
 })
 airchie14.addEventListener('click', ()=>{
     airchie14.classList.toggle('gamwvanda')
     airchie13.classList.remove('gamwvanda')
     airchie15.classList.remove('gamwvanda')
+    if(airchie14.classList.contains('gamwvanda')){
+        res5.textContent = '"Every 2 weeks"'
+    }else{
+        res5.textContent = ""
+    }
 })
 airchie15.addEventListener('click', ()=>{
     airchie15.classList.toggle('gamwvanda')
     airchie14.classList.remove('gamwvanda')
     airchie13.classList.remove('gamwvanda')
+    if(airchie15.classList.contains('gamwvanda')){
+        res5.textContent = '"Every month"'
+    }else{
+        res5.textContent = ""
+    }
 })
+
